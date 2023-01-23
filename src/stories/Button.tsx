d3b4a61fdcb1349eaaff4a8ca504a3659fbe939c
SadrtdinovAI@ufa.infteh.ru <SadrtdinovAI@ufa.infteh.ru>
2023-01-23 18:57:18 +0500

    configured tailwind and storybook

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import './button.css';
-
 interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
@@ -38,8 +35,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+    className="bg"
       {...props}
     >
       {label}
