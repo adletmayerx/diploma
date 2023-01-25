2a8a5715a32ff9b181eac4a670fb59bbc93b5747
asadrtdinov <asadrtdinov2003@gmail.com>
2023-01-25 18:56:53 +0500

    feat: continue to create Button component

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import clsx from "clsx";
 
 type ButtonProps = {
   text: string;
-  className?: string;
+  type: "general" | "transparent";
+  className: string;
 };
 
-const Button = ({ text, className = "" }: ButtonProps) => {
+const Button = ({ text, type = "general", className }: ButtonProps) => {
   return (
     <button
-      className={`inline-flex h-11 w-96 cursor-pointer items-center justify-center rounded bg-blue-600 text-gray-50 transition duration-300 ease-in hover:opacity-80 ${className}`}
+      className={clsx(
+        "inline-flex cursor-pointer items-center justify-center rounded text-gray-50 transition duration-300 ease-in hover:opacity-80 ",
+        {
+          "h-11 w-96 bg-blue-600 ": type === "general",
+          "h-9 w-32 opacity-60": type === "transparent",
+        },
+        className && className
+      )}
     >
       {text}
     </button>
