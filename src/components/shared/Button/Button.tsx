53560035fa09a908bcb6399ea2cb67972d28783b
asadrtdinov <asadrtdinov2003@gmail.com>
2023-01-27 18:43:16 +0500

    feat: added login type to ButtonComponent

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 type ButtonProps = {
   text: string;
-  type: "general" | "transparent";
+  type: "general" | "transparent" | "login";
   className: string;
 };
 
@@ -10,10 +10,11 @@ const Button = ({ text, type = "general", className }: ButtonProps) => {
   return (
     <button
       className={clsx(
-        "inline-flex transform cursor-pointer items-center justify-center rounded text-gray-50 transition duration-300 ease-in hover:opacity-80 ",
+        "inline-flex transform cursor-pointer items-center justify-center rounded text-xs text-gray-50 transition duration-300 ease-in hover:opacity-80",
         {
           "h-11 w-96 bg-blue-600 ": type === "general",
-          "h-9 w-32 opacity-60": type === "transparent",
+          "h-9 w-32 text-gray-900 opacity-60": type === "transparent",
+          "h-6 w-14 bg-green-400 text-gray-900": type === "login",
         },
         className && className
       )}
