73e15cf0eff7987c99e9b5b599625ccf2c5bc949
asadrtdinov <asadrtdinov2003@gmail.com>
2023-03-16 09:12:47 +0500

    feat: finished Header component for HomePage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,16 +8,16 @@ const Header = () => {
 
   return (
     <header
-      className={clsx("flex items-center justify-between px-4 py-5", [
+      className={clsx("flex items-center justify-between px-4 py-5 md:px-8 xl:px-16", [
         router.pathname === "/" ? "bg-tiber" : "bg-mine-shaft",
       ])}
     >
       <MemoLogoIcon />
-      <div className="flex justify-start gap-4">
-        <button className="text-xs font-medium leading-relaxed text-gray-50">
+      <div className="flex justify-start gap-4 md:gap-8">
+        <button className="text-xs font-medium text-gray-50">
           Регистрация
         </button>
-        <button className="h-7 w-14 rounded bg-shamrock px-3 py-1 text-xs font-medium leading-relaxed text-gray-900">
+        <button className="h-7 w-14 rounded bg-shamrock text-xs font-medium leading-relaxed text-gray-900 align-middle md:h-8 md:w-20">
           Войти
         </button>
       </div>
