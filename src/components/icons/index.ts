72e2c3d5cc9427b2278b53e216ebb25212666d7d
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2023-04-28 05:12:45 +0500

    feat: add FavouritePage markup and styles

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,10 +2,12 @@ import MemoHeaderLogoIcon from "./HeaderLogoIcon";
 import MemoLinkArrowIcon from "./LinkArrowIcon";
 import MemoHeartIcon from "./HeartIcon";
 import MemoHeartFilledIcon from "./HeartFilledIcon";
+import MemoRemoveIcon from "./RemoveIcon";
 
 export {
   MemoHeaderLogoIcon,
   MemoLinkArrowIcon,
   MemoHeartIcon,
   MemoHeartFilledIcon,
+  MemoRemoveIcon,
 };
--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next";
-import { Footer, Movies, Search } from "~/components";
+import { Footer, Movies, Search, MoviesCard } from "~/components";
+import { Button } from "~/components/shared";
 
-const MoviesPage: NextPage = () => {
+const FavouritesPage: NextPage = () => {
   const MOVIES = [
     {
       image: "/movies-image.png",
@@ -44,11 +45,18 @@ const MoviesPage: NextPage = () => {
     <div className="flex flex-col items-center bg-mine-shaft">
       <div className="flex w-full max-w-7xl flex-col">
         <Search />
-        <Movies movies={MOVIES} />
+        <Movies movies={MOVIES} CardComponent={MoviesCard} cardType={"favorites"} />
+        <Button
+          className={
+            "mx-auto mt-12 mb-20 flex h-9 w-60 items-center justify-center rounded-md bg-dark-charcoal text-xs font-medium text-gray-50 md:w-80"
+          }
+        >
+          Ещё
+        </Button>
         <Footer />
       </div>
     </div>
   );
 };
 
-export default MoviesPage;
+export default FavouritesPage;
