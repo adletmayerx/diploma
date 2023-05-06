ffb4c8b7b7a0caacc3635d4cfb785a22684ffbfa
adletmayerx <asadrtdinov2003@gmail.com>
2023-05-06 11:36:09 +0500

    feat: configure CredentialsProvider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,10 +7,12 @@ import {
 import DiscordProvider from "next-auth/providers/discord";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
+import { loginSchema } from "~/validation/auth";
+import { verify } from "argon2";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -40,14 +42,27 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  jwt: { secret: "super-secret", maxAge: 15 * 24 * 30 * 60 },
+  pages: {
+    signIn: "/login",
+    newUser: "/register",
+  },
   callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+
+      return token;
+    },
+
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
         // session.user.role = user.role; <-- put other properties on the session here
       }
       if (session.expires) {
-        
       }
 
       return session;
@@ -67,39 +82,42 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    // CredentialsProvider({
-    //   // The name to display on the sign in form (e.g. 'Sign in with...')
-    //   name: 'Credentials',
-    //   // The credentials is used to generate a suitable form on the sign in page.
-    //   // You can specify whatever fields you are expecting to be submitted.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   // You can pass any HTML attribute to the <input> tag through the object.
-    //   credentials: {
-    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
-    //     password: { label: "Password", type: "password" }
-    //   },
-    //   async authorize(credentials, req) {
-    //     // You need to provide your own logic here that takes the credentials
-    //     // submitted and returns either a object representing a user or value
-    //     // that is false/null if the credentials are invalid.
-    //     // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-    //     // You can also use the `req` object to obtain additional parameters
-    //     // (i.e., the request IP address)
-    //     const res = await fetch("/your/endpoint", {
-    //       method: 'POST',
-    //       body: JSON.stringify(credentials),
-    //       headers: { "Content-Type": "application/json" }
-    //     })
-    //     const user = await res.json()
-  
-    //     // If no error and we have user data, return it
-    //     if (res.ok && user) {
-    //       return user
-    //     }
-    //     // Return null if user data could not be retrieved
-    //     return null
-    //   }
-    // })
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        const parsedCredentials = await loginSchema.parseAsync(credentials);
+        const user = await prisma.user.findFirst({
+          where: { email: parsedCredentials.email },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isValidPassword = await verify(
+          user.password as string,
+          parsedCredentials.password
+        );
+
+        if (!isValidPassword) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
     /**
      * ...add more providers here.
      *
