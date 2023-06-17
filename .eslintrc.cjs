e27ba72dddcfb7af94110e6790cc6b59d6dc1d98
adletmayerx <asadrtdinov2003@gmail.com>
2023-06-17 11:10:45 +0500

    feat: add rule to use async functions in onClick and onSubmit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,11 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [2, {
+      "checksVoidReturn": {
+        "attributes": false
+      }
+    }]
   },
 };
 
