150744b43fe63a89c49f16189ac8e031d79b0008
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-21 11:35:16 +0500

    feat: start Header authenticated state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,14 @@ import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
-import MemoLogoIcon from "./icons/HeaderLogoIcon";
+import { MemoHeaderLogoIcon, MemoProfileIcon } from "./icons";
 import ModalMenu from "./ModalMenu";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
   const router = useRouter();
-  const [isModalVisible, setIsModalVisible] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const onModalCloseButtonClick = () => {
     setIsModalVisible(false);
@@ -19,25 +21,56 @@ const Header = () => {
         router.pathname === "/" ? "bg-tiber" : "bg-mine-shaft",
       ])}
     >
-      <div className="flex w-full max-w-7xl items-center justify-between px-4 py-5 md:px-8 xl:px-16">
-        <MemoLogoIcon
-          className="cursor-pointer"
+      <div
+        className={clsx(
+          "flex w-full max-w-7xl items-center gap-11 px-4 py-5 md:px-8 xl:px-16",
+          data ? "justify-start" : "justify-between"
+        )}
+      >
+        <MemoHeaderLogoIcon
+          className="cursor-pointer flex-shrink-0"
           onClick={() => router.push("/")}
         />
-        <div className="flex justify-start gap-4 md:gap-8">
-          <Link
-            className="inline-flex items-center justify-center text-xs font-medium text-gray-50"
-            href={"/register"}
-          >
-            Регистрация
-          </Link>
-          <Link
-            className="inline-flex h-7 w-14 items-center justify-center rounded bg-shamrock align-middle text-xs font-medium leading-relaxed text-gray-900 md:h-8 md:w-20"
-            href={"/login"}
-          >
-            Войти
-          </Link>
-        </div>
+        {data ? (
+          <div className="flex w-full justify-between">
+            <div className="flex justify-start gap-4 md:gap-8">
+              <Link
+                className="inline-flex items-center justify-center text-xs font-medium text-gray-50"
+                href={"/movies"}
+              >
+                Фильмы
+              </Link>
+              <Link
+                className="inline-flex items-center justify-center text-xs font-medium text-gray-50"
+                href={"/favorites"}
+              >
+                Сохранённые фильмы
+              </Link>
+            </div>
+            <Link
+              className="inline-flex items-center justify-center gap-4 text-xs font-medium text-gray-50"
+              href={"/profile"}
+            >
+              <span>Аккаунт</span>
+              <MemoProfileIcon />
+            </Link>
+          </div>
+        ) : (
+          <div className="flex justify-start gap-4 md:gap-8">
+            <Link
+              className="inline-flex items-center justify-center text-xs font-medium text-gray-50"
+              href={"/register"}
+            >
+              Регистрация
+            </Link>
+            <Link
+              className="inline-flex h-7 w-14 items-center justify-center rounded bg-shamrock align-middle text-xs font-medium leading-relaxed text-gray-900 md:h-8 md:w-20"
+              href={"/login"}
+            >
+              Войти
+            </Link>
+          </div>
+        )}
       </div>
       <ModalMenu
         isVisible={isModalVisible}
