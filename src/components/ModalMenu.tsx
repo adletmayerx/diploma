fcf37eeea48aa5dff53f8ae8b2381ccbf41ea060
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-21 14:29:01 +0500

    feat: add ProfilePage link and some styling

--- src/components/ModalMenu.tsx
+++ src/components/ModalMenu.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Button } from "./shared";
-import { MemoCloseIcon } from "./icons";
+import { MemoCloseIcon, MemoProfileIcon } from "./icons";
 import Link from "next/link";
 import clsx from "clsx";
 import { createPortal } from "react-dom";
@@ -16,10 +16,8 @@ const ModalMenu = ({ isVisible, onCloseButtonClick, pathname }: Props) => {
     { href: "/", title: "Главная" },
     { href: "/movies", title: "Фильмы" },
     { href: "/favorites", title: "Сохраненные фильмы" },
-  ];
-
-  const active = "/movies";
-
+    { href: "/profile", title: "Аккаунт" },
+  ] as const;
   const [mounted, setMounted] = React.useState(false);
 
   useEffect(() => setMounted(true), []);
@@ -35,15 +33,25 @@ const ModalMenu = ({ isVisible, onCloseButtonClick, pathname }: Props) => {
 
   return mounted
     ? createPortal(
-        <div className={clsx("absolute top-0 left-0 flex h-screen w-full flex-col overflow-hidden", !isVisible && "pointer-events-none")}>
-          <div className={clsx("relative flex h-full w-full flex-col items-center justify-start bg-mine-shaft pt-36 transition-transform duration-300", !isVisible && 'translate-x-full' )}>
+        <div
+          className={clsx(
+            "absolute top-0 left-0 flex h-screen w-full flex-col overflow-hidden",
+            !isVisible && "pointer-events-none"
+          )}
+        >
+          <div
+            className={clsx(
+              "relative flex h-full w-full flex-col items-center justify-start bg-mine-shaft pt-36 pb-24 transition-transform duration-700",
+              !isVisible && "translate-x-full"
+            )}
+          >
             <Button
               className="absolute right-4 top-4"
               onClick={onCloseButtonClick}
             >
               <MemoCloseIcon />
             </Button>
-            <ul className="flex flex-col items-center justify-start gap-6">
+            <ul className="flex h-full flex-col items-center justify-start gap-6">
               {LINKS.map((link) => {
                 return (
                   <li
@@ -51,10 +59,20 @@ const ModalMenu = ({ isVisible, onCloseButtonClick, pathname }: Props) => {
                     className={clsx(
                       "relative cursor-pointer text-lg font-medium text-gray-50",
                       pathname === link.href &&
-                        "after:absolute after:top-full after:left-0 after:h-0.5 after:w-full after:bg-gray-50"
+                        "after:absolute after:top-8 after:left-0 after:h-0.5 after:w-full after:bg-gray-50",
+                      link.href === "/profile" && "mt-auto"
                     )}
                   >
-                    <Link href={link.href}>{link.title}</Link>
+                    <Link
+                      href={link.href}
+                      className={clsx(
+                        link.href === "/profile" &&
+                          "inline-flex items-center justify-center gap-2"
+                      )}
+                    >
+                      {link.title}{" "}
+                      {link.href === "/profile" && <MemoProfileIcon />}
+                    </Link>
                   </li>
                 );
               })}
