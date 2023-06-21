6376482299b37c7cb2dbd5549fbda0491d721b41
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-21 10:10:35 +0500

    feat: add ModalMenu logic

--- src/components/ModalMenu.tsx
+++ src/components/ModalMenu.tsx
@@ -6,11 +6,12 @@ import clsx from "clsx";
 import { createPortal } from "react-dom";
 
 type Props = {
-  onClose: () => unknown;
-  title: string;
+  isVisible: boolean;
+  onCloseButtonClick: () => unknown;
+  pathname: string;
 };
 
-const ModalMenu = () => {
+const ModalMenu = ({ isVisible, onCloseButtonClick, pathname }: Props) => {
   const LINKS = [
     { href: "/", title: "Главная" },
     { href: "/movies", title: "Фильмы" },
@@ -23,18 +24,23 @@ const ModalMenu = () => {
 
   useEffect(() => setMounted(true), []);
   useEffect(() => {
-    document.body.style.overflow = "hidden";
+    if (isVisible) {
+      document.body.style.overflow = "hidden";
 
-    return () => {
-      document.body.style.overflow = "";
-    };
-  });
+      return;
+    }
+
+    document.body.style.overflow = "";
+  }, [isVisible]);
 
   return mounted
     ? createPortal(
-        <div className="absolute top-0 left-0 flex h-screen w-screen flex-col">
-          <div className="relative flex h-full w-full flex-col items-center justify-start bg-mine-shaft pt-36">
-            <Button className="absolute right-4 top-4">
+        <div className={clsx("absolute top-0 left-0 flex h-screen w-full flex-col overflow-hidden", !isVisible && "pointer-events-none")}>
+          <div className={clsx("relative flex h-full w-full flex-col items-center justify-start bg-mine-shaft pt-36 transition-transform duration-300", !isVisible && 'translate-x-full' )}>
+            <Button
+              className="absolute right-4 top-4"
+              onClick={onCloseButtonClick}
+            >
               <MemoCloseIcon />
             </Button>
             <ul className="flex flex-col items-center justify-start gap-6">
@@ -44,8 +50,8 @@ const ModalMenu = () => {
                     key={link.title}
                     className={clsx(
                       "relative cursor-pointer text-lg font-medium text-gray-50",
-                      active === link.title &&
-                        "after:absolute after:top-4 after:h-px after:w-full after:bg-gray-50"
+                      pathname === link.href &&
+                        "after:absolute after:top-full after:left-0 after:h-0.5 after:w-full after:bg-gray-50"
                     )}
                   >
                     <Link href={link.href}>{link.title}</Link>
