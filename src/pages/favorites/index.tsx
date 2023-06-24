c6222407bf2296549288018d7d7611644428c970
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-24 14:48:16 +0500

    feat: add route protection to Movies and Favorites pages

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,8 +1,16 @@
-import { type NextPage } from "next";
+import {
+  type GetServerSideProps,
+  type InferGetServerSidePropsType,
+  type NextPage,
+} from "next";
+import { type Session, getServerSession } from "next-auth";
 import { Footer, Movies, Search, MoviesCard } from "~/components";
 import { Button } from "~/components/shared";
+import { authOptions } from "~/server/auth";
 
-const FavouritesPage: NextPage = () => {
+const FavouritesPage: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const MOVIES = [
     {
       image: "/movies-image.png",
@@ -45,7 +53,11 @@ const FavouritesPage: NextPage = () => {
     <div className="flex flex-col items-center bg-mine-shaft">
       <div className="flex w-full max-w-7xl flex-col">
         <Search />
-        <Movies movies={MOVIES} CardComponent={MoviesCard} cardType={"favorites"} />
+        <Movies
+          movies={MOVIES}
+          CardComponent={MoviesCard}
+          cardType={"favorites"}
+        />
         <Button
           className={
             "mx-auto mt-12 mb-20 flex h-9 w-60 items-center justify-center rounded-md bg-dark-charcoal text-xs font-medium text-gray-50 md:w-80"
@@ -57,6 +69,27 @@ const FavouritesPage: NextPage = () => {
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<{
+  data: Session;
+}> = async (context) => {
+  const data = await getServerSession(context.req, context.res, authOptions);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default FavouritesPage;
